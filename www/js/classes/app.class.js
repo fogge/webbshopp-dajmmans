3967924feb311c807bc29a10aef29c8d7f9fd50e
Markus Andersson <make_99@hotmail.com>
2018-03-07 13:42:29 +0100

    fixed and working backend so we can load in from database

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -2,21 +2,14 @@ class App extends REST {
 
   constructor() {
     super();
-    $.ajax({
-      url: '/ingredients',
-      success: (data) => {
-        console.log("Reset of database", data);
-        // Now run our tests
-        this.myIngredients = data;
-        this.start();
-      }
-    });
+    this.start();
 
   }
 
-  start() {
+  async start() {
     // Initiate handling of SPA push/pop-state
-console.log(this.myIngredients);
+    this.myIngredients = await Ingredient.find();
+    console.log(this.myIngredients);
   //  new PopStateHandler(this);
   }
 
