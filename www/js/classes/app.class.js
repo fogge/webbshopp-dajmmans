15db25064b6c4ff82fd880a9a85aaa14dd09dd33
Niklas Jansson <Niklas.Jansson91@hotmail.com>
2018-03-09 11:12:31 +0100

    Produkter renderas ut på startsidan.

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -15,17 +15,15 @@ class App extends REST {
     this.header.render('header');
 
     //Rendering the footer
-
+    this.footer = new Footer(this);
+    $('footer').empty();
+    this.footer.render('footer');
+    
     this.startPage = new Startpage(this);
     this.materiel = new Materiel(this);
     this.ingredienser = new Ingredient(this);
     this.bocker = new Bocker(this);
     this.about = new About(this);
-
-    // Footer 
-    this.footer = new Footer(this);
-    $('footer').empty();
-    this.footer.render('footer');
 
     this.popState = new PopStateHandler(this);
   }
