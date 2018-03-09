5da85fb8dd6e234a7b506d26fbfcaae1bf92b411
Unknown <lecke.martin@gmail.com>
2018-03-09 12:58:31 +0100

    Tries to fix searchresult with REST.find()

--- www/js/classes/REST.class.js
+++ www/js/classes/REST.class.js
@@ -1,4 +1,4 @@
-class REST extends Base {
+class REST extends Base{
 
   constructor(obj){
     super();
@@ -23,14 +23,33 @@ class REST extends Base {
   }
 
   static async find(query){
+
+    if(typeof query == 'object'){
+      query = JSON.stringify(query,(key,val) => {
+        if(val && val.constructor === RegExp){
+          val = val + '';
+          val = val.split('/');
+          let op = val.pop();
+          val = val.join('/');
+          val = val.substr(1, val.length - 1);
+
+          val = {$regex: val, $options: op};
+        }
+        return val;
+      });
+    }
     let entity = (this.name + 's').toLowerCase();
     let results = await REST.request(entity,'GET',query,'');
-    console.log(results);
+    await console.log('result', results);
+    let orgresults = results;
     results = results.result || [results];
+    delete orgresults.result;
     let enriched = [];
     for(let result of results){
       enriched.push(new this(result));
     }
+    enriched.info = orgresults;
+    await console.log('enriched', enriched);
     return enriched;
   }
 
@@ -47,7 +66,7 @@ class REST extends Base {
   static async request(entity, reqMethod, query={}, body={}){
 
     let reqObj = {
-      url: `/${entity}/${JSON.stringify(query)}`, // entity for example "books"
+      url: `/${entity}/${query}`, // entity for example "books"
       method: reqMethod, // POST, GET, PUT, DELETE
       dataType: 'json', // I except JSON back from the server
       data: JSON.stringify(body), // JSON to send to server
@@ -60,6 +79,7 @@ class REST extends Base {
     if(reqMethod != "POST" && reqMethod != "PUT"){
       delete reqObj.data;
     }
+
     return await $.ajax(reqObj);
 
   }
--- www/js/classes/search.class.js
+++ www/js/classes/search.class.js
@@ -8,11 +8,13 @@ class Search extends REST {
   async getSearchResult() {
     let query = Search.searchQuery;
     // Hardcoded search on ingredients collection with title
-    let searchResultFromMongo = await Ingredient.request('ingredients', 'get', {title: Search.searchQuery});
-    searchResultFromMongo.result.forEach( (product) => {
-      this.searchResult.push(new ProductAvatar(product));
-    })
-    return this.render();
+    // let searchResultFromMongo = await Ingredient.request('ingredients', 'GET', `title[$regex]=${Search.searchQuery}`);
+    let searchResultFromMongo = await Ingredient.find('title=Blodkiss');
+    await console.log('searchResultFromMongo', searchResultFromMongo);
+    // searchResultFromMongo.result.forEach( (product) => {
+    //   this.searchResult.push(new ProductAvatar(product));
+    // })
+    return await this.render();
   }
 }
 
