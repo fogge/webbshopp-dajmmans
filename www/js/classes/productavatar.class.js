380b4025f4a5a95234a377ce0ede81a47b63ff9c
Unknown <lecke.martin@gmail.com>
2018-03-11 20:34:30 +0100

    You can now add item into Cart "search"-results

--- www/js/classes/productavatar.class.js
+++ www/js/classes/productavatar.class.js
@@ -1,8 +1,16 @@
 class ProductAvatar extends REST {
-  constructor(product) {
+  constructor(product, app) {
     super();
+    this.app = app;
     for (let value in product) {
       this[value] = product[value];
     }
+  }
+  click() {
+    if ($(event.target).hasClass('addToCart')) {
+      this.app.cart.push(this);
+      this.app.header.render();
+    }
+    
   }
 }
