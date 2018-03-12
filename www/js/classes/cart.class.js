56d59539f6d52f93679417c1fef81b16a02e3844
Unknown <andreas@stickydirt.se>
2018-03-12 11:12:28 +0100

    Now showing VAT in shopping cart

--- www/js/classes/cart.class.js
+++ www/js/classes/cart.class.js
@@ -4,7 +4,6 @@ class Cart extends REST {
     this.app = app;
     this.cartItems = this.app.shoppingCart;
     this.render();
-
   }
 
   newInstances() {
@@ -12,5 +11,30 @@ class Cart extends REST {
       this.cartItems.push(new CartItem(item, this.app));
     });
     this.render();
+  }
+
+
+  getTotalPrice(){
+    let totalPrice = 0;
+    for (let cartItem of this.cartItems) {
+      totalPrice += cartItem.price;
+    }
+    return totalPrice;
+  }
+
+  getTotalVat(){
+    let totalVat = 0;
+
+    for (let cartItem of this.cartItems) {
+      // Calculating vat off the set price
+      if (cartItem.vatRate == 6) {
+        totalVat += cartItem.price * 0.0566;
+      } else if (cartItem.vatRate == 12) {
+        totalVat += cartItem.price * 0.1071;
+      } else {
+        totalVat += cartItem.price * 0.2;
+      }
+    }
+    return Math.round( totalVat * 10 ) / 10;
   }
 }
