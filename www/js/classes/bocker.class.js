ada50c603665087178c5a7cc209bc86e283626db
Markus Andersson <make_99@hotmail.com>
2018-03-13 15:40:43 +0100

    Added product descriptions and new images for each category

--- www/js/classes/bocker.class.js
+++ www/js/classes/bocker.class.js
@@ -3,6 +3,12 @@ class Book extends REST {
     super();
     this.result = result;
     Book.title = 'Böcker';
+    Book.breadText = `
+    Här hos Dajmmans erbjuder vi det mest unika utbudet av böcker för den magiska världen. Vårt utbud utökas kontinuerligt, så skulle vi inte ha en produkt på lager så kontakta oss via mail eller telefon.
+    `;
+    Book.img = '/img/books.jpg';
+    Book.icon = `<i class="fas fa-book"></i>`;
   }
+
 
 }
