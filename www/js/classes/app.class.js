16518672b1265781d184a87d996c19d970f746bd
Unknown <lecke.martin@gmail.com>
2018-03-13 15:51:35 +0100

    Removed products page problem

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -23,7 +23,7 @@ class App extends REST {
     this.materiel = new Materiel(this);
     this.ingredienser = new Ingredient(this);
     this.Book = new Book(this);
-    this.productPage = new ProductPage(this);
+    // this.productPage = new ProductPage(this);
     this.about = new About(this);
 
     // Footer 
