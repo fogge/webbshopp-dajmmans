6399557f659d4b789a00f73a2325f17d9ec10ab5
Johan Anderberg <johan_anderberg79@hotmail.com>
2018-03-14 22:42:55 +0100

    Mina sidor new component

--- www/js/classes/user-page.class.js
+++ www/js/classes/user-page.class.js
@@ -54,3 +54,26 @@ class Userpage extends REST {
 	};
   	
   }
+
+
+  /*
+  Gammal userpage som funkar!!!
+  Spara tills den nya funkar...
+  Object.assign(Userpage.prototype, { template1(){ return `
+<div class="my-4">
+  <div class="row">
+    <div class="col-12">
+    	<h3 class="text-center mt-5 mb-5">Du är inloggad som: ${this.user}</h3>
+    	<h5 class="mb-3">Dina aktuella ordrar:</h5>
+      <div id="accordion" class="active-orders">
+      	${this.activeOrders.length == 0 ? 'Du har inga aktuella ordrar, beställ genast!' : this.activeOrders.render()}
+			<h5 class="mb-3 mt-5">Orderhistorik:</h5>
+      <div id="accordion" class="history-orders">
+      	${this.oldOrders.length == 0 ? 'Du har ingen orderhistorik än!' : this.oldOrders.render()}
+			</div>
+
+    </div>
+  </div>
+</div>
+`;}});
+*/
