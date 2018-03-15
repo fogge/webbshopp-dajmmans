82b81f9562076a910b36021cd9b5860a39707202
Roger <rogge_acdc@hotmail.com>
2018-03-15 10:34:04 +0100

    export fil

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static('www'));
 const Ingredient = require('./classes/ingredient.class');
 const Book = require('./classes/book.class');
 const Materiel = require('./classes/materiel.class');
+const mailer = require('/classes/sendmail.class')
 
 let ingredient = new Ingredient(app);
 let book = new Book(app);
@@ -27,41 +28,7 @@ ingredient.setupImportRoute(ingredientJson);
 book.setupImportRoute(booksJson);
 materiel.setupImportRoute(materielJson);
 
-app.post('/send-email', function (req, res) {
-  nodemailer.createTestAccount((err, account) => {
-      let transporter = nodemailer.createTransport({
-          host: 'smtp.ethereal.email',
-          port: 587,
-          secure: false, // true for 465, false for other ports 
-          auth: {
-              user: account.user, // generated ethereal user 
-              pass: account.pass // generated ethereal password 
-          }
-      });
-      console.log('transporter',transporter);
-      // setup email data with unicode symbols 
-      let mailOptions = {
-          from: '"Dajmans webbshop 👻"', // sender address 
-          to: req.body.to, // list of receivers 
-          subject: req.body.subject, // Subject line 
-          text: '', // plain text body 
-          html: '<h1>Du har beställt följande</h1>' // html body 
-      };
-
-      // send mail with defined transport object 
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message sent: %s', info.messageId);
-          // Preview only available when sending through an Ethereal account 
-          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com> 
-          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou... 
-      });
-  });
-});
+app.post('/send-email', mailer);
 
 app.get(/^[^\.]*$/, (req, res) => {
  res.sendFile(__dirname + '/www/index.html');
