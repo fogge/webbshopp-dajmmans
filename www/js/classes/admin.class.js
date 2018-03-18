71f83507b060ede8d2baedeeef8bc65ef2f0c15b
André Tente <iamandretente@gmail.com>
2018-03-19 00:45:10 +0100

    update admin, dynamic fill of progress bar, style

--- www/js/classes/admin.class.js
+++ www/js/classes/admin.class.js
@@ -3,6 +3,7 @@ class Admin extends REST {
       super();
       this.app = app;
       this.getOrders({});
+      this.clickEvents();
     }
 
     async getOrders(searchObj) {
@@ -11,6 +12,37 @@ class Admin extends REST {
         this.order = order;
         this.order.result.orderdate = this.order.result.orderdate.substring(0,10);
         this.render('.orderList',2);
+        this.orderStatus();
+      }
+    }
+
+    clickEvents(){
+      $(document).on('click', '#orderDetails', function( event ) {
+        let state = $(event.target).attr('value');
+        state == 'Open'? $(event.target).attr('value', 'Close').text('Stänga detaljer') : $(event.target).attr('value', 'Open').text('Öppna detaljer')
+      });
+    }
+
+    orderStatus(){
+      switch(this.order.result.status){
+        case 'Beordrade': 
+          $(`#orderStatus-1-${this.order.result._id}`).addClass('d-block');
+          break;
+        case 'Behandlad': 
+          $(`#orderStatus-1-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-2-${this.order.result._id}`).addClass('d-block');
+          break;
+        case 'Skickad': 
+          $(`#orderStatus-1-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-2-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-3-${this.order.result._id}`).addClass('d-block');
+          break;
+        case 'Mottagen': 
+          $(`#orderStatus-1-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-2-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-3-${this.order.result._id}`).addClass('d-block');
+          $(`#orderStatus-4-${this.order.result._id}`).addClass('d-block');
+          break;
       }
     }
   }
